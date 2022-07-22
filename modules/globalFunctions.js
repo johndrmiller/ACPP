e0677a9d82e1a8c5777025eaf9f83ce6badb39a2
John Miller <johndrmiller@gmail.com>
2022-07-21 22:07:12 -0400

    update import links

--- modules/globalFunctions.js
+++ modules/globalFunctions.js
@@ -1,4 +1,4 @@
-import { svPicker } from "/modules/HTMLconstants.js";
+import { svPicker } from "./HTMLconstants.js";
 export function assign(ele, obj) {
     let keys = Object.keys(obj);
     keys.forEach(x => {
